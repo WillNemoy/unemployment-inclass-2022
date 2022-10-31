0605a2f5843caa0b9a95e0123ee9108ab8a27f2c
WillNemoy <98560701+WillNemoy@users.noreply.github.com>
2022-10-31 10:16:25 -0400

    Challenge A: find the latest unemployment rate

--- app/unemployment.py
+++ app/unemployment.py
@@ -1,6 +1,7 @@
 
 #This is the "app/unemployment_.py" file...
 import os
+from pickle import FALSE
 import pandas as pd
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
@@ -9,6 +10,9 @@ request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={
 
 df = pd.read_csv(request_url)
 
-print(df.head())
-print(df.columns)
-print(len(df))
+#print(df.head())
+
+#Challenge A
+print("Latest Unemployment Rate:")
+df_first_row = df.iloc[0]
+print(f"{df_first_row['timestamp']}:", f"{df_first_row['value']}%")
